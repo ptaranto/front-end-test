e365c24935a66d20f8b9c22f0962e8ab9506a3bf
Pedro Taranto <github@pedrotaranto.com.br>
2019-08-08 11:07:11 +0100

    displaying customer name, expected time and gravatar image in QueueScreen

--- src/Queue/QueueScreen.js
+++ src/Queue/QueueScreen.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
 import base64 from 'base-64';
+import Customer from './components/Customer';
 
 export default class extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customers: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    const headers = new Headers();
+    headers.set(
+      'Authorization',
+      'Basic ' + base64.encode('codetest1:codetest100')
+    );
+    fetch('api/queue/gj9fs', {
+      method: 'GET',
+      headers
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          customers: json.queueData.queue.customersToday
+        });
+      });
+  }
+
+  render() {
+    let customers = [];
+    let customer;
+    for (let i = 0; i < this.state.customers.length; ++i) {
+      customer = this.state.customers[i];
+      customers.push(
+        <div key={customer.id}>
+          <Customer customer={customer} />
+        </div>
+      );
     }
 
-    componentDidMount() {
-        const headers = new Headers();
-        headers.set('Authorization', 'Basic ' + base64.encode("codetest1:codetest100"));
-        fetch('api/queue/gj9fs', {
-            method: 'GET',
-            headers
-        }).then(response => response.json())
-            .then(json => {
-                this.setState({
-                    customers: json.queueData.queue.customersToday
-                })
-            });
-    }
-
-    render() {
-        let customers = [];
-        for(let i = 0; i < this.state.customers.length; ++i) {
-            customers.push(
-                <div key={this.state.customers[i].id}>
-                    <div>
-                        {this.state.customers[i].customer.name}
-                    </div>
-                </div>
-            );
-        }
-
-        return (
-            <div>
-                {
-                    customers
-                }
-            </div>
-        );
-    }
+    return <div>{customers}</div>;
+  }
 }
--- src/Queue/components/Customer.js
+++ src/Queue/components/Customer.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import md5 from 'md5';
 import CustomerCard from './CustomerCard';
 import ProfilePicture from './ProfilePicture';
 import Name from './Name';
 import Content from './Content';
+import Label from './Label';
 
-export default () =>
+export default props => {
+  const { currentPosition, expectedTime } = props.customer;
+  const { emailAddress, name } = props.customer.customer;
+  let gravatarId = emailAddress ? emailAddress.trim().toLowerCase() : '';
+  const hash = md5(gravatarId);
+
+  return (
     <CustomerCard>
-        <ProfilePicture />
-        <Content>
-            <Name></Name>
-            <div></div>
-        </Content>
-    </CustomerCard>;
+      <ProfilePicture
+        style={{
+          backgroundImage: `url(https://www.gravatar.com/avatar/${hash}?s=290)`
+        }}
+      />
+      <Content>
+        <Name>{`Pos #${currentPosition} Name: ${name}`}</Name>
+        <Label>{new Date(expectedTime).toLocaleString()}</Label>
+      </Content>
+    </CustomerCard>
+  );
+};
